ca7d10b2d244752c2fc1c0acc0223ed89ab9d70a
sagarparker <32793769+sagarparker@users.noreply.github.com>
2020-05-26 20:44:26 +0530

    Before the 2nd asign

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -12,5 +12,6 @@ export const routes: Routes = [
   { path: 'menu',     component: MenuComponent },
   { path: 'about', component:AboutComponent},
   { path: 'contact', component:ContactComponent},
+  { path: 'dishdetail/:id',     component: DishdetailsComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
--- src/app/dishdetails/dishdetails.component.ts
+++ src/app/dishdetails/dishdetails.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit , Input} from '@angular/core';
 import { Dish } from '../shared/dish';
-
+import { DishService } from '../services/dish.service';
+import { Params, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-dishdetails',
@@ -9,12 +11,19 @@ import { Dish } from '../shared/dish';
 })
 export class DishdetailsComponent implements OnInit {
 
-  @Input()
-  dish = Dish;
+  dish: Dish;
   
-  constructor() { } 
+  constructor(private dishService: DishService,
+    private route: ActivatedRoute,
+    private location: Location) { }
 
   ngOnInit() {
+    let id = this.route.snapshot.params['id'];
+    this.dish = this.dishService.getDish(id);
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 
 }
